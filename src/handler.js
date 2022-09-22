42801f10383f77483f6e49d3e0d74dcdf1933ae9
Tyler Hasenoehrl <tylrhas@gmail.com>
2022-09-22 14:55:49 -0700

    mock code

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,26 @@
+const reportGetters = require('./database/get/report');
+const clinicGetters = require('./database/get/clinic');
+const utils = require('./utils');
+const { isReportSendable } = utils;
+const { getClinicIntegrationSettings } = clinicGetters;
+const { getReportById } = reportGetters;
+
 module.exports.handler = async event => {
     console.log(event);
+    const { reportId, clinicId } = event;
+    const clinicSettings = await getClinicIntegrationSettings(clinicId);
+    if (!clinicSettings.isRedoxIntegrationEnabled) {
+        console.log(
+            `Redox integration is not enabled for clinicId ${clinicId}`
+        );
+        return;
+    }
+    const report = await getReportById(reportId);
+    if (isReportSendable(report, clinicSettings)) {
+        // check that the coversheet dates match
+        // get clinics media destinations
+        // check if there are pending or failed transmissions
+        // queue the redox transmission
+        // write a row in the transmissions table
+    }
 };
