75c238e1e55622cd295ff948bf719a8fd3ac468b
Brad Cook <48299228+CookBrad@users.noreply.github.com>
2022-09-30 09:50:57 -0500

    add schemas dir (#7)

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const REPORT_UPDATE_STREAM_NAME = 'report-update';
+TEST_SCHEMA_DIR = 'schemas'
 module.exports = {
-    REPORT_UPDATE_STREAM_NAME
+    REPORT_UPDATE_STREAM_NAME,
+    TEST_SCHEMA_DIR
 };
